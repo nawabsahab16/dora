8b5a9a9413e3a990a9356e59eea493e1f4727835
nawabsahab16 <Nawab162004@gmail.com>
2024-10-10 01:34:32 +0530

    feat:mobile responsive(mq)

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,4 +1,6 @@
 
+import clsx from "clsx";
+import { useState } from "react";
 import {Link as LinkScroll} from "react-scroll";
 
 const NavLink = ({title}) => (
@@ -9,18 +11,21 @@ const NavLink = ({title}) => (
 
 )
 
-
-
 const Header = () => {
+
+     const [isOpen, setIsOpen] = useState(false);
+
+
   return (
     <header className="fixed top-0 left-0 z-50 w-full py-10">
      <div className="container flex h-14 items-center max-lg:px-5"> 
         <a className="lg:hidden flex-1 cursor-pointer z-2">
            <img src="/images/xora.svg" width={105} height={50} alt="logo" /> 
         </a>
-     
-       <div className="w-full border-2 border-amber-400">
-        <nav >
+        
+        <div className={clsx("w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0", isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none",)}>
+       <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+        <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
             <ul className="flex max-lg:block max-lg:px-12">
                 <li className="nav-li">
                    <NavLink title="features" />
@@ -43,9 +48,17 @@ const Header = () => {
             </ul>
         </nav>
        </div>
-  
-
      </div>
+       <button className="lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full 
+       flex justify-center items-center"
+           onClick={() => setIsOpen((prevState) => !prevState)}
+       > 
+           <img src={`/images/${isOpen ? "close" : "magic"}.svg`}
+            alt = "magic"
+            className="size-1/2 object-contain"
+           />
+       </button>
+    </div>
     </header>    
 
 )
